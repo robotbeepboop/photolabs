9177f21711ff281b3ff6a84de2496307b4f18014
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-17 13:23:13 -0700

    import photolist

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import PhotoList from './components/PhotoList';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => (
   <div className="App">
-    <PhotoListItem/>
+    <PhotoListItem />
   </div>
 )
 
