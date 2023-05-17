04f0e0ce03a267515f30f5a061bb0ef6e400fe95
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-17 12:58:06 -0700

    create initial props react output

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,15 +1,19 @@
-
 import React from 'react';
-
 import '../styles/PhotoListItem.scss';
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  /* initial props display */
+  { props.hideUserName === true && <><p>{ props.imageSource }</p><p>{ props.id }</p></>}
+  <>
+    <p>{ props.username }</p>
+    <p>{ props.imageSource }</p>
+    <p>{ props.id }</p>
+  </>
 }
 
 PhotoListItem.defaultProps = {
   username: 'Jacob',
-  imageSource: `${process.env.PUBLIC_URL}/Image.jpg`,
+  imageSource: `${ process.env.PUBLIC_URL }/Image.jpg`,
   id: 1,
   hideUserName: false,
 }
