45e68e7badb59dd05d7628ac3a5e080b0c131f47
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-17 13:29:13 -0700

    put items in div for organization

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,12 +3,12 @@ import '../styles/PhotoListItem.scss';
 
 const PhotoListItem = (props) => {
   /* initial props display */
-  { props.hideUserName === true && <><p>{ props.imageSource }</p><p>{ props.id }</p></>}
-  <>
+  { props.hideUserName === true && <div><p>{ props.imageSource }</p><p>{ props.id }</p></div>}
+  <div className='photo-list--item'>
     <p>{ props.username }</p>
     <p>{ props.imageSource }</p>
     <p>{ props.id }</p>
-  </>
+  </div>
 }
 
 PhotoListItem.defaultProps = {
