38f94a8a7e7b31c19d0930f138783fbeefa3f51a
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-18 12:19:42 -0700

    display topic list items

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-
 import './TopicList.scss';
+import TopicListItem from './TopicListItem';
 
-const TopicList = () => {
-  <div className="top-nav-bar--topic-list">
-    {/* Insert React */}
-  </div>
+const TopicList = (props) => {
+  const mappedTopics = props.topics.map((topic) => {
+    return <TopicListItem 
+    key={ topic.id }
+    label={ topic.label }
+    link={ topic.link }/>;
+  });
+
+  return (
+    <div className="top-nav-bar--topic-list">
+      { mappedTopics }
+    </div>
+  ); 
+
 }
 
 TopicList.defaultProps = {
