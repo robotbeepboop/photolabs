af6da3e82d23fcad5c43fbafdad4f2c02fed8b61
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-18 12:22:03 -0700

    display topic list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PhotoList from './components/PhotoList';
+import TopicList from './components/TopicList';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => (
   <div className="App">
+    <TopicList />
     <PhotoList />
   </div>
 )
