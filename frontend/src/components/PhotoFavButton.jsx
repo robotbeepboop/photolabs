4a43b664b2d375ddda959da68f4a623a5db72ee4
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-17 22:47:22 -0700

    finalize button
    got the button to display finally

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,14 @@ import { FavIcon } from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  /* deconstruction array for usestate, default is false so that a user may 'like' it later*/
+  const [ fav, setFav ] = useState(false);
+  
   return (
-    <div className="photo-list--fav-icon">
+    /* change fav state to true and make the favicon red */
+    <div className="photo-list--fav-icon" onClick={(() => {setFav(!fav);, [ fav ])}>
       <div className="photo-list--fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon fill={ fav === true ? '#800020' : '#D3D3D3' } />
       </div>
     </div>
   );
