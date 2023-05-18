ffc7bb4469a72ff2c5b5a7f666528464258bc2a7
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-18 14:23:45 -0700

    display home page and nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList';
+import React, { useState } from 'react';
+import TopNavigation from './components/TopNavigationBar';
 import './App.scss';
+import HomeRoute from './components/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
-const App = () => (
-  <div className="App">
-    <TopicList />
-    <PhotoList />
-  </div>
-)
+const App = () => {
+  const [likes, setLikes] = useState(0);
+
+  return (
+    <div className="App">
+    <TopNavigation likes={ likes } />
+    <HomeRoute setLikes={ setLikes } />
+  </div>);
+  
+}
 
 export default App
