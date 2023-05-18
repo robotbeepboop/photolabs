78db52d972bf40fd194326475994d8d26f5ccda5
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-18 14:54:09 -0700

    add likes

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import FavBadge from './components/FavBadge';
+import TopicList from './TopicList';
+import topics from '../mocks/topics'
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = () => {
+const TopNavigation = (props) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar--logo">PhotoLabs</span>
+      <TopicList topics={topics}/>
+      <FavBadge />
     </div>
   )
 }
