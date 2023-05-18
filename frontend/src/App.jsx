1fa4341dbbefc25333b3bfc353364023828014d4
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-17 22:29:56 -0700

    display list, not individual item

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import PhotoList from './components/PhotoList';
-import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => (
   <div className="App">
-    <PhotoListItem />
+    <PhotoList />
   </div>
 )
 
