1a649da6ab52cffa832301ce8cd228cbafc5eca7
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-19 09:33:12 -0700

    clean up code from last night

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import photos from '../mocks/photos';
 import PhotoList from './components/PhotoList';
 import './HomeRoute.scss';
