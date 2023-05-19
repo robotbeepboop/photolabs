7939c1ed7779f4c0c57442acab8a49919b1351f4
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-19 09:30:34 -0700

    Update TopicList.jsx

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -14,27 +14,7 @@ const TopicList = (props) => {
     <div className="top-nav-bar--topic-list">
       { mappedTopics }
     </div>
-  ); 
-
+  );
 }
 
-TopicList.defaultProps = {
-  topics: [
-    {
-      id: 1,
-      label: 'Nature',
-      link: 'link placeholder' 
-    },
-    { 
-      id: 2, 
-      label: 'Food',
-      link: 'link placeholder' 
-    },
-    {
-      id: 3,
-      label: 'People',
-      link: 'link placeholder' 
-    },
-  ]
-}
 export default TopicList
