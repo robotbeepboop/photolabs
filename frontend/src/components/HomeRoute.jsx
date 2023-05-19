4aace2203ec0672ae83809eb3995288cd86c1555
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-19 09:41:05 -0700

    move likes func to home route

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import photos from '../mocks/photos';
-import PhotoList from './components/PhotoList';
-import './HomeRoute.scss';
+import TopNavigationBar from '../components/TopNavigationBar';
+import PhotoList from '../components/PhotoList';
+import '../styles/HomeRoute.scss';
 
-//render home page with the photos
 const HomeRoute = (props) => {
-  return (
-    <div className='home-route'>
-      <div className='photo-list'>
-        <PhotoList props={photos} setLikes={props.setLikes} />
-      </div>
-    </div>
-  )
-};
+  /* use object or array to store likes [tried array, try object next] */
+  const [ likes, setLikes ] = useState({});
 
-export default HomeRoute
+  const checkLikes = (id) => {
+    if (!likes[id]) {
+      //if photo is not liked, add it to the likes object
+      setLikes({ ...likes, [id]: true })
+    }
+    setLikes({ ...likes, [id]: false });
+  };
+
+  return(
+    <div className="home-route">
+      <TopNavigationBar topics={ props.topics } />
+      <PhotoList photos={ props.photos } likes={ likes } checkLikes={ checkLikes } />
+    </div>
+  );
+}
+export default HomeRoute;
