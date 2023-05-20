7377fe07b6c4e7c8a9112e7bae30bc268a4b9583
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-19 18:42:53 -0700

    remove comment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import React, { useState } from 'react';
 import './App.scss';
 import HomeRoute from './components/HomeRoute';
-/*mock data */
 import photoData from './mocks/photos.json';
 import topicData from './mocks/topics.json';
 
-// Note: Rendering a single component to build components in isolation
-
-const App = () => {
+const App = () =>{
+  const [favourites, setFavourites] = useState(0);
   return (
     <div className='App'>
-      <HomeRoute photos={photoData} topics={topicData} />
+      <HomeRoute photos={ photoData } topics={ topicData } favourites={ favourites } setFavourites={ setFavourites } />
     </div>
   );
 }
