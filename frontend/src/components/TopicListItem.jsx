9ae96977661d82517afa3cc4b7d8d7fdf00ec56f
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-19 18:36:31 -0700

    remove defactoring

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import '../styles/TopicListItem'
+import '../styles/TopicListItem.scss';
 
 const TopicListItem = (props) => {
   /*extract the label for the topic */
-  const { id, label, link } = props;
-
   return (
     <div className="topic-list--item">
-      <h3>{ label }</h3>
-    </div>);
+      <h3>{ props.label }</h3>
+    </div>
+  );
   
-}
-
-TopicListItem.defaultProps = {
-  id: 1,
-  label: 'Nature',
-  link: 'link placeholder' 
 }
 export default TopicListItem
