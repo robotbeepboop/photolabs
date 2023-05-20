a6ee492d134097e5bd7cd209d0b00abd608a4f3d
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-19 18:41:32 -0700

    add modal

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -4,19 +4,12 @@ import PhotoListItem from './PhotoListItem';
 
 const PhotoList = (props) => {
   const mappedPhotos = props.photos.map((photo) => {
-    return <PhotoListItem 
-    username={ photo.user.username }
-    imageSource={ photo.urls.thumb }
-    key={ photo.id }
-    id={ photo.id }
-    hideUserName={ photo.hideUserName }
-    likes={ props.likes }
-    checkLikes={ props.checkLikes }/>;
+    return <PhotoListItem username={ photo.user.username } imageSource={ photo.urls.thumb } key={ photo.id } id={ photo.id } onClick={ props.clickHandler } />;
   });
-
+  
   return (
     <ul className="photo-list">
-    { mappedPhotos }
+      { mappedPhotos }
     </ul>
   ); 
 }
