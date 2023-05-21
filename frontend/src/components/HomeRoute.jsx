7fd8354fb041c74934e6930e5008f234c82c1e73
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-21 12:28:25 -0700

    begin centralizing functions

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -4,22 +4,36 @@ import PhotoList from '../components/PhotoList';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = (props) => {
-  /* use object or array to store likes [tried array, try object next] */
-  const [ likes, setLikes ] = useState({});
-
-  const checkLikes = (id) => {
-    if (!likes[id]) {
-      //if photo is not liked, add it to the likes object
-      setLikes({ ...likes, [id]: true })
+  const [favourites, setFavourites] = useState([]); //use array, not object. remember class example ***
+  const [viewModal, setViewModal] = useState(false);
+  
+  /* add or remove a photo from favourites array */
+  const toggleFavs = (photo_id) => {
+    const newFavourites = [];
+    if  (!favourites.includes(photo_id)) { //spread operator to gather the rest of the likes
+      newFavourites = [...favourites, photo_id];
+    } else {
+      newFavourites = favourites.filter(photo => photo !== photo_id);
     }
-    setLikes({ ...likes, [id]: false });
+    //change the favourites state
+    setFavourites(newFavourites);
   };
-
+  
+  /* modal toggle funcs */
+  const openModalHandler = () => {
+     setViewModal(true);
+  };
+  
+  const closeModalHandler = () => {
+    setViewModal(false);
+  };
+  
   return(
     <div className="home-route">
       <TopNavigationBar topics={ props.topics } />
-      <PhotoList photos={ props.photos } likes={ likes } checkLikes={ checkLikes } />
+      <PhotoList photos={ props.photos } />
     </div>
   );
-}
+};
+
 export default HomeRoute;
