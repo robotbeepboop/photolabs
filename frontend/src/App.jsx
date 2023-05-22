4a851e0ddc1a93db5e95495d8ece12af66845ce7
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-22 08:52:02 -0700

    remove state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
 import HomeRoute from './components/HomeRoute';
 import photoData from './mocks/photos.json';
