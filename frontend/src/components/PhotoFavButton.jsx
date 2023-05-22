233783cdb140d26f35f8c508c1f4199b9ba9e823
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-21 18:03:18 -0700

    clean up liked function

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,15 +1,14 @@
 import React, { useCallback, useState } from 'react';
-
 import { FavIcon } from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton(props) {
-  /* deconstruction array for usestate, default is false so that a user may 'like' it later*/
-  const { id, likes, checkLikes } = props;
+  //state for whether a photo is liked by the user or not
+  const [isLiked, setIsLiked] = useState(false);
+
   return (
-    /* change fav state to true and make the favicon red */
-    <div className="photo-list--fav-icon" onClick={() => checkLikes(id)}>
-      <FavIcon fill={ likes ? '#800020' : '#D3D3D3' } />
+    <div className="photo-list--fav-icon" onClick={() => setIsLiked(!isLiked)}>
+      <FavIcon fill={ isLiked ? '#800020' : '#D3D3D3' } />
     </div>
   );
 }
