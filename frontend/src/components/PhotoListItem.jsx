ca1b07ca8a456e47c8040aa2e9371ee74180529a
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-23 15:03:14 -0700

    finalize list item

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,18 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/PhotoListItem.scss';
-import FavPhotoButton from './FavFavPhotoButton';
+import PhotoFavButton from './PhotoFavButton';
 
 const PhotoListItem = (props) => {
   const clickHandler = () => {
-    viewModal();
+    props.viewModal();
+    props.selectPhoto(photo_id);
   };
-  
+
   return (
     <div className='photo-list--item'>
-      <button onClick={ clickHandler }>
-        <FavPhotoButton />
-      </button>
-      <img className='photo-list--image' src={ props.imageSource } alt={ props.photo.description } onClick={ props.clickHandler(props.photo) } />
+      <PhotoFavButton toggleFavs={ toggleFavs } photo={ photo_id } />
+      <img className='photo-list--image' src={ props.imageSource } alt={ props.photo.description } onClick={ clickHandler } />
       <p>{ props.photo.user.username }</p>
       <p>Location: { props.photo.location.city }, { props.photo.location.country }</p>
     </div>
