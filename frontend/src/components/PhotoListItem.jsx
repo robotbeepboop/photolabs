c3727fc8323b07cf6c884bac6b06e1386f654d2e
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-23 15:11:30 -0700

    fix names

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -11,9 +11,9 @@ const PhotoListItem = (props) => {
   return (
     <div className='photo-list--item'>
       <PhotoFavButton toggleFavs={ toggleFavs } photo={ photo_id } />
-      <img className='photo-list--image' src={ props.imageSource } alt={ props.photo.description } onClick={ clickHandler } />
-      <p>{ props.photo.user.username }</p>
-      <p>Location: { props.photo.location.city }, { props.photo.location.country }</p>
+      <img className='photo-list--image' src={ props.imageSource } alt={ props.alt } onClick={ clickHandler } />
+      <p>{ props.username }</p>
+      <p>Location: { props.city }, { props.country }</p>
     </div>
   );
 }
