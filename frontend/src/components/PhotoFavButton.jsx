eb8f01fc3207bc7cb0b9a2193a0fc8f17a93bfbf
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-23 15:16:47 -0700

    refactor likes function

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,14 +1,18 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import { FavIcon } from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton(props) {
-  //state for whether a photo is liked by the user or not
-  const [isLiked, setIsLiked] = useState(false);
+  const [liked, setLiked] = useState(false);
+
+  const clickHandler = () => {
+    setLiked(!liked);
+    props.toggleFavs(photo_id);
+  };
 
   return (
-    <div className="photo-list--fav-icon" onClick={() => setIsLiked(!isLiked)}>
-      <FavIcon fill={ isLiked ? '#800020' : '#D3D3D3' } />
+    <div className="photo-list--fav-icon" onClick={ clickHandler }>
+      <FavIcon fill={ liked ? '#800020' : '#D3D3D3' } />
     </div>
   );
 }
