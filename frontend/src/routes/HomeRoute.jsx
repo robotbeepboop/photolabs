81d9db967fdd168c592901aa22b522d9a6d48d84
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-23 14:43:13 -0700

    add modal

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import PhotoList from '../components/PhotoList';
+import React, { useState } from 'react';
 import TopNavigationBar from '../components/TopNavigationBar';
+import PhotoList from '../components/PhotoList';
+import PhotoDetailsModal from './PhotoDetailsModal';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = (props) =>
-    return (
-      <div className='home-route'>
-        <TopNavigationBar topics={ props.topics }/>
-        <PhotoList photos={ props.photos }/>
-      </div>
-    );
-  </div>
-
+const HomeRoute = (props) => {
+  return(
+    <div className="home-route">
+      { props.viewModal && <PhotoDetailsModal photos={ props.photos } selectedPhoto={ props.selectedPhoto } favourites={ props.favourites } closeModalHandler={ props.closeModalHandler } />}
+      <TopNavigationBar topics={ props.topics } favPhotoExist={ props.favPhotoExist } selectTopic={ props.selectTopic } />
+      <PhotoList photos={ props.photos } selectPhoto={ props.selectPhoto } favourites={ props.favourites } viewModal={ props.viewModal } />
+    </div>
+  );
+}
 export default HomeRoute;
