0c210aa05bfa25628c3b6f28e7a4a69fd43d3bca
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-23 14:59:38 -0700

    fix naming

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -7,9 +7,9 @@ import '../styles/HomeRoute.scss';
 const HomeRoute = (props) => {
   return(
     <div className="home-route">
-      { props.viewModal && <PhotoDetailsModal photos={ props.photos } selectedPhoto={ props.selectedPhoto } favourites={ props.favourites } closeModalHandler={ props.closeModalHandler } />}
+      { props.viewModal && <PhotoDetailsModal photos={ props.photos } selectedPhoto={ props.selectedPhoto } toggleFavs={ props.toggleFavs } closeModalHandler={ props.closeModalHandler } />}
       <TopNavigationBar topics={ props.topics } favPhotoExist={ props.favPhotoExist } selectTopic={ props.selectTopic } />
-      <PhotoList photos={ props.photos } selectPhoto={ props.selectPhoto } favourites={ props.favourites } viewModal={ props.viewModal } />
+      <PhotoList photos={ props.photos } selectPhoto={ props.selectPhoto } toggleFavs={ props.toggleFavs } viewModal={ props.viewModal } />
     </div>
   );
 }
