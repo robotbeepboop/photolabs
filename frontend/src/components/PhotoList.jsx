9c9385ad7b787ff17b3e67684fa5d87eb0833779
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-23 15:11:06 -0700

    edit naming convention

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
+import '../styles/PhotoList.scss';
 import PhotoListItem from './PhotoListItem';
 
 const PhotoList = (props) => {
-  const mappedPhotos = photos.map(photo => {
-    <PhotoListItem username={ photo.user.username } imageSource={ photo.urls.thumb } key={ photo.id } id={ photo.id } />
+  const mappedPhotos = props.photos.map((photo) => {
+    return <PhotoListItem username={ photo.user.username } imageSource={ photo.urls.thumb } key={ photo.id } id={ photo.id } alt={ photo.description } city={ photo.location.city } country={ photo.location.country } />;
   });
+  
   return (
-    <ul>
-    { mappedPhotos }
+    <ul className="photo-list">
+      { mappedPhotos }
     </ul>
-  );
-};
+  ); 
+}
 
-export default HomeRoute;
+export default PhotoList
